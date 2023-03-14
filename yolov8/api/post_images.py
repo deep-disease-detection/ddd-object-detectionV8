0f32a9852b7565d23ae50b57a46961712412b866
Youssef El Ouard <youselouard@gmail.com>
2023-03-14 22:14:30 +0100

    Working version for predictions in production

--- yolov8/api/post_images.py
+++ yolov8/api/post_images.py
@@ -2,11 +2,11 @@ import requests
 import json
 import base64
 
-url = "http://localhost:8000/detect/"
-# url2 = à récupérer après
+url = "http://localhost:8000/predict/"
+url2 = "https://ddd-object-detection-unyu5blyja-ew.a.run.app/predict/"
 
 
-with open('yolov8/data/Adenovirus_A4-65k-071120_2.jpg', 'rb') as f:
+with open('yolov8/data/Papilloma_1547.jpg', 'rb') as f:
     image = f.read()
     # transform the image into a JSON string
     image_str = base64.b64encode(image).decode("utf-8")
@@ -16,6 +16,6 @@ with open('yolov8/data/Adenovirus_A4-65k-071120_2.jpg', 'rb') as f:
     message_str = json.dumps(message_dict)
     # Send the message to the API
     print('sending image base64')
-    response = requests.post(url, json=message_dict)
+    response = requests.post(url2, json=message_dict)
 
     print(response.json())
--- yolov8/api/utils.py
+++ yolov8/api/utils.py
@@ -3,7 +3,7 @@ import time
 import os
 
 
-def save_image_to_bucket_gcp(image, bucket, virus):
+def save_image_to_bucket_gcp(image, bucket, virus, virus_count):
     """Save image to bucket in GCP
     Args:
          image (bytes): Image to be saved
@@ -16,6 +16,6 @@ def save_image_to_bucket_gcp(image, bucket, virus):
     name_photo = name_photo.split(".")[0] + "-" + str(time.time()) + ".jpg"
     blob = bucket.blob(os.path.basename(name_photo))
     # add metadata to image
-    blob.metadata = {"time": str(time.time()), "class": virus}
+    blob.metadata = {"time": str(time.time()), "class": virus, "virus_count":virus_count}
     blob.upload_from_string(image, content_type="image/jpeg")
     print("Image saved to bucket")
