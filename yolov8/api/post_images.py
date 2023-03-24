43008b07dd17fea6a620b6def92b9e896b4aedd1
Youssef El Ouard <youselouard@gmail.com>
2023-03-24 11:49:02 +0100

    Fixed minor issues with global variables to avoid setting them in each script

--- yolov8/api/post_images.py
+++ yolov8/api/post_images.py
@@ -2,11 +2,9 @@ import requests
 import json
 import base64
 
-url = "http://localhost:8000/predict/"
-url2 = "https://ddd-object-detection-unyu5blyja-ew.a.run.app/predict/"
+url = "http://localhost:8000/predict/"  # to test locally
 
-
-with open('yolov8/data/Adenovirus_A4-65k-071120_2.jpg', 'rb') as f:
+with open("yolov8/data/Adenovirus_A4-65k-071120_2.jpg", "rb") as f:
     image = f.read()
     # transform the image into a JSON string
     image_str = base64.b64encode(image).decode("utf-8")
@@ -15,7 +13,7 @@ with open('yolov8/data/Adenovirus_A4-65k-071120_2.jpg', 'rb') as f:
     # Convert the dictionary to a JSON string
     message_str = json.dumps(message_dict)
     # Send the message to the API
-    print('sending image base64')
-    response = requests.post(url2, json=message_dict)
+    print("sending image base64")
+    response = requests.post(url, json=message_dict)
 
     print(response.json())
